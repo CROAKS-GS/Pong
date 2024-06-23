509909894e418ae154a773cd87f12385bd826c00
MyGuyMyWay <snakesun0522@gmail.com>
2024-06-23 14:22:12 -0700

    ball actually bounces

--- PongChamp/ball.gd
+++ PongChamp/ball.gd
@@ -1,13 +1,30 @@
-extends RigidBody2D
+extends CharacterBody2D
 var speed = 400
 var screen_size = Vector2.ZERO
+var hit = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
 	hide()
-
-
+	
+func start(num):
+	$CollisionShape2D.disabled = false
+	position = Vector2(960, randi_range(300, 780))
+	show()
+	var rng = randi() % 2
+	if(rng == 0):
+		rng = -1
+	velocity.x = 500 * rng	
+	velocity.y = randi_range(-500, 500)
+	
+func bounce():
+	velocity.x = velocity.x * -1
+	
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if((position.y <= 0 and velocity.y < 0) or (position.y >= 1080 and velocity.y > 0)):
+		velocity.y = velocity.y * -1	
+		
+	position += velocity * delta
--- PongChamp/player_1.gd
+++ PongChamp/player_1.gd
@@ -1,4 +1,5 @@
 extends Area2D
+signal playerhit
 
 var speed = 400
 var screen_size = Vector2.ZERO
@@ -24,3 +25,7 @@ func _process(delta):
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 62.5, screen_size.y - 62.5)
+
+
+func _on_body_entered(body):
+	playerhit.emit()
