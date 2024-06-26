b4bca856d2e862ba2868c17f5e6a36db76d14b41
Ryan Lay <30853424+totally-not-frito-lays@users.noreply.github.com>
2024-06-26 16:46:10 -0700

    updated collision box to actually fit player
    seems like the collision box wasn't actually being programmatically assigned to be the same as the actual polygon like what was done previously in the heartbeast platformer tutorial series.

--- PotatoPong/Player.gd
+++ PotatoPong/Player.gd
@@ -5,11 +5,12 @@ const ACCELERATION = 800.0
 const FRICTION = 1000.0
 
 @onready var collision_polygon_2d = $CollisionPolygon2D
-@onready var polygon_2d = $Polygon2D
+@onready var polygon_2d = $CollisionPolygon2D/Polygon2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	collision_polygon_2d.polygon = polygon_2d.polygon
+	#polygon_2d.polygon = collision_polygon_2d.polygon
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
