0b1f723564ea502e285828e888aaf460d3f5490c
Ryan Lay <30853424+totally-not-frito-lays@users.noreply.github.com>
2024-06-26 17:32:46 -0700

    gravity strategy (not working lol)

--- PotatoPong/Ball.gd
+++ PotatoPong/Ball.gd
@@ -3,8 +3,8 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var prev_collision_counter = 0
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -14,7 +14,9 @@ func _physics_process(delta):
 	handle_collision()
 	
 func handle_collision():
-	for i in get_slide_collision_count():
-		var collision = get_slide_collision(i)
-		print("Collided with: ", collision.get_collider().name)
-	
+	#for i in get_slide_collision_count():
+		#var collision = get_slide_collision(i)
+		#print("Collided with: ", collision.get_collider().name)
+	if prev_collision_counter == get_slide_collision_count():
+		prev_collision_counter += 1
+		gravity = gravity * -1
